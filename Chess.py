f3d3054c0ee50bca983c3da885ab1f80a0947d06
Ecstatik <johankool2003@hotmail.co.uk>
2023-09-19 11:10:28 +0100

    Fixed starting positions

--- Chess.py
+++ Chess.py
@@ -1,7 +1,7 @@
 # This will serve as the gameplay loop for the Chess Engine
 import ChessGUI
 
-importPath = "Resources/Data/StartPositions-TestingPromotion"
+importPath = "Resources/Data/StartPositions"
 
 GUI = ChessGUI.GraphicalUserInterface(importPath)  # This is responsible for all interactions
 running = True
